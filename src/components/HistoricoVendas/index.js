c3245bcb141870aec88b721d56e0f6f15208621c
Jardel Costa <jfcosta@ic.ufal.br>
2020-11-29 20:39:12 -0300

    Adicionando historico de vendas

--- src/components/HistoricoVendas/index.js
+++ src/components/HistoricoVendas/index.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import React, { useState, useEffect } from "react";
+import { Row, Col, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import api from "../../services/api";
 
 function HistoricoVendas() {
+  const [products, setProduct] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await api.get("/sold");
+      console.log(res.data);
+      setProduct(res.data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
-      <Helmet>
-        <title>All Bertinho - Apresentação</title>
-      </Helmet>
-      <p>
-        Histórico de vendas
-      </p>
+      <Row>
+        {products.map(p => (
+          <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
+            <Card className="rounded my-3 p-3">
+              <Link to={`/product/${p._id}`}>
+                <Card.Img src={p.image} variant="top"></Card.Img>
+              </Link>
+              <Card.Body>
+                <Link>
+                  <Card.Title>
+                    <strong>{p.name}</strong>
+                  </Card.Title>
+                </Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </>
   )
 }
