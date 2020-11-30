03440f1913e455b020ee32be790b86e2326b6f1b
Marcela Mancino <marcelamancino@pm.me>
2020-11-30 14:42:47 -0300

    sei lá ein

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
 
- baseURL: 'http://allbertinho.herokuapp.com/',
+ baseURL: 'https://allbertinho.herokuapp.com/',
  //  baseURL: 'http://localhost:3030', //colocar a porta usada no de vcs
   // baseURL: 'http://localhost:3333',
 
