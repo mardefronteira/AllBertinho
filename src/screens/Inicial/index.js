55179dffbdf37e140c1f97664d26b3729508d19a
Marcela Mancino <marcelamancino@pm.me>
2020-11-30 17:57:06 -0300

    padronização dos cards da landing

--- src/screens/Inicial/index.js
+++ src/screens/Inicial/index.js
@@ -8,14 +8,14 @@ import { Row, Col, Card } from "react-bootstrap";
 
 function Inicial() {
     const [products, setProduct] = useState([]);
-  
+
     useEffect(() => {
       const fetchProducts = async () => {
         const res = await api.get("/product");
         console.log(res.data);
         setProduct(res.data);
       };
-  
+
       fetchProducts();
     }, []);
 
@@ -33,7 +33,7 @@ function Inicial() {
       <Row>
       <Col sm={12} md={6} lg={4} xl={3}>
             <Card className="rounded my-3 p-3">
-                <Card.Body> 
+                <Card.Body>
                 <Card.Title>
                   <strong>Cone Estiloso</strong>
                 </Card.Title>
@@ -44,7 +44,7 @@ function Inicial() {
         </Col>
         <Col sm={12} md={6} lg={4} xl={3}>
             <Card className="rounded my-3 p-3">
-                <Card.Body> 
+                <Card.Body>
                 <Card.Title>
                   <strong>Mesa de jantar</strong>
                 </Card.Title>
@@ -54,15 +54,15 @@ function Inicial() {
               </Card>
         </Col>
         <Col sm={12} md={6} lg={4} xl={3}>
-            <Card className="rounded my-3 p-3">
-                <Card.Body> 
-                <Card.Title>
-                  <strong>Blusinha QRCODE</strong>
-                </Card.Title>
-                </Card.Body>
-                <Card.Img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2CyePO2q0yyxKyR5hbmtQwHaI4%26pid%3DApi&f=1" variant="top"></Card.Img>
-                <Card.Img src="https://i.ibb.co/nf0HsNv/bola-Quadrada.png" variant="top"></Card.Img>
-              </Card>
+          <Card className="rounded my-3 p-3">
+            <Card.Body>
+              <Card.Title>
+                <strong>Fusca azul</strong>
+              </Card.Title>
+            </Card.Body>
+            <Card.Img src="https://i.pinimg.com/originals/1c/0a/f3/1c0af31f2c35968cfb8770b1521b7d1d.jpg" variant="top"></Card.Img>
+            <Card.Img src="https://i.ibb.co/pxgVdb6/fusca-Azul.png" variant="top"></Card.Img>
+          </Card>
         </Col>
         <Col sm={12} md={6} lg={4} xl={3}>
             <Card className="rounded my-3 p-3">
@@ -76,33 +76,32 @@ function Inicial() {
             </Card>
         </Col>
         <Col sm={12} md={6} lg={4} xl={3}>
-            <Card className="rounded my-3 p-3">
-              <Card.Body>
-                <Card.Title>
-                  <strong>Fusca azul</strong>
-                </Card.Title>
-                </Card.Body>
-                <Card.Img src="https://leianoticias.com.br/wp-content/uploads/2017/08/carro-fusca-azul-0817-1400x800.jpg" variant="top"></Card.Img>
-                <Card.Img src="https://i.ibb.co/pxgVdb6/fusca-Azul.png" variant="top"></Card.Img>
-              </Card>
+          <Card className="rounded my-3 p-3">
+            <Card.Body>
+              <Card.Title>
+                <strong>Box Naruto</strong>
+              </Card.Title>
+            </Card.Body>
+            <Card.Img src="https://images-shoptime.b2w.io/produtos/01/00/img/1514787/6/1514787621_2SZ.jpg" variant="top"></Card.Img>
+            <Card.Img src="https://i.ibb.co/4PJJGFy/naruto.png" variant="top"></Card.Img>
+          </Card>
         </Col>
-
         <Col sm={12} md={6} lg={4} xl={3}>
             <Card className="rounded my-3 p-3">
-                <Card.Body> 
+                <Card.Body>
                 <Card.Title>
-                  <strong>Box Naruto</strong>
+                  <strong>Blusinha QRCODE</strong>
                 </Card.Title>
                 </Card.Body>
-                <Card.Img src="https://images-shoptime.b2w.io/produtos/01/00/img/1514787/6/1514787621_2SZ.jpg" variant="top"></Card.Img>
-                <Card.Img src="https://i.ibb.co/4PJJGFy/naruto.png" variant="top"></Card.Img>
+                <Card.Img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2CyePO2q0yyxKyR5hbmtQwHaI4%26pid%3DApi&f=1" variant="top"></Card.Img>
+                <Card.Img src="https://i.ibb.co/nf0HsNv/bola-Quadrada.png" variant="top"></Card.Img>
               </Card>
-        </Col>        
+        </Col>
       </Row>
       {
         /***
          * <Row>
-      {products ? 
+      {products ?
         products.map(p => (
           <Col sm={12} md={6} lg={4} xl={3}>
               <Card className="rounded my-3 p-3">
@@ -115,13 +114,13 @@ function Inicial() {
                   <Card.Img src={`https://allbertinho.herokuapp.com//qrcode/${p._id}`} variant="top"></Card.Img>
                 </Card>
           </Col>
-                
+
         ))
       : <p></p>
-      } </Row>  
+      } </Row>
          */
       }
-      
+
       </main>
     </>
   )
